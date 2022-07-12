ed7e36a043f51e3c29544030e0332c36eb302c6e
MLBartley <docbartley@gmail.com>
2022-07-11 18:12:13 -0600

    Script updates, added overview of data sources.

--- scripts/00.01-LoadData.R
+++ scripts/00.01-LoadData.R
@@ -8,43 +8,47 @@
 ## notes ---------------------------
 ## 
 ##   - Data received on July 5, 2022
+##   - Data updated (trail_count and weather) on July 11, 2022
 ##
 ## ---------------------------
 
 
 # load packages -----------------------------------------------------------
+library(here) #helps to use relative file paths within the R project
 library(readxl) #useful for loading excel files into R
 library(lubridate) #helps with date (day/month/year) data formating
+
 # load data ---------------------------------------------------------------
 
 # load strava data
-strava_day <- readxl::read_excel("data/raw/strava_day.xlsx")
-strava_month <- readxl::read_excel("data/raw/strava_month.xlsx")
-strava_year <- readxl::read_excel("data/raw/strava_year.xlsx")
+strava_day <- readxl::read_excel(here("data/raw/strava_day.xlsx"))
+strava_month <- readxl::read_excel(here("data/raw/strava_month.xlsx"))
+strava_year <- readxl::read_excel(here("data/raw/strava_year.xlsx"))
 
 #load trail data
-trail_char <- readxl::read_excel("data/raw/trailcharacteristics.xlsx")
-trail_count <- readxl::read_excel("data/raw/trailcounterdata.xlsx")
+trail_char <- readxl::read_excel(here("data/raw/trailcharacteristics.xlsx"))
+trail_count <- readxl::read_excel(here("data/raw/trailcounterdata.xlsx"))
 
 #load relational data table joining strava and trail count IDs - multiple sheets
-join_IDs <- readxl::read_excel("data/raw/Trailhead_Strava_Crosswalk.xlsx", sheet = 1)
-join_IDs_allEdges <- readxl::read_excel("data/raw/Trailhead_Strava_Crosswalk.xlsx", 
+join_IDs <- readxl::read_excel(here("data/raw/Trailhead_Strava_Crosswalk.xlsx"), sheet = 1)
+join_IDs_allEdges <- readxl::read_excel(here("data/raw/Trailhead_Strava_Crosswalk.xlsx"), 
                                             sheet = 3)
 
 #load weather data
-weather <- readxl::read_excel("data/raw/weather_airquality.xlsx")
+weather <- readxl::read_excel(here("data/raw/weather_airquality.xlsx"))
 
 
 # process data ------------------------------------------------------------
 
 # remove extraneous rows from tables (check with tail(<objectname>) to see if NA)
-join_IDs <- join_IDs[, -(27:30)]
+join_IDs <- join_IDs[ -(27:30),]
 
 # check data types; change to date (day/month/year)
 strava_day$timeframe <- lubridate::as_date(strava_day$timeframe)
 trail_count$date <- lubridate::as_date(trail_count$date)
 weather$date <- lubridate::as_date(weather$date)
-trail_count$DayOfWeek <- lubridate::wday(trail_count$date, label=T)
+trail_count$wday <- lubridate::wday(trail_count$date, label=T)
+strava_day$wday <- lubridate::wday(strava_day$timeframe, label=T)
 
 # add fake day to make it easier to change to date format
 strava_month$timeframe <- paste(strava_month$timeframe, "-01", sep="")
@@ -55,4 +59,5 @@ strava_month$month <- lubridate::month(strava_month$timeframe)
 strava_year$timeframe <- paste(strava_year$timeframe, "-01-01", sep="")
 strava_year$timeframe <- lubridate::as_date(strava_year$timeframe)
 strava_year$year <- lubridate::year(strava_year$timeframe)
+
 
